4d108b55a4f8dbd2b53980143cb2c3b0c5568dd6
Kumbukani Kamanga <kkamanga16@gmail.com>
2022-01-10 16:16:01 +0300

    playing with controller files

--- schooldb/src/api/student/controllers/student.js
+++ schooldb/src/api/student/controllers/student.js
@@ -1,9 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  *  student controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::student.student');
+module.exports = createCoreController("api::student.student", ({ strapi }) => ({
+  // Method 1: Creating an entirely custom action
+  async exampleAction(ctx) {
+    try {
+      ctx.body = ctx;
+      console.log(ctx.body);
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
