9734e7e16055b5597ed8ead021bea5e402132fa8
Kumbukani Kamanga <kkamanga16@gmail.com>
2022-01-11 23:45:16 +0300

    WIP - change string to json

--- schooldb/src/api/student/controllers/student.js
+++ schooldb/src/api/student/controllers/student.js
@@ -8,16 +8,18 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 const validateLogin = (userDetails) => {
   // use KN
-  return 401;
+  return 202;
 };
 
 module.exports = createCoreController("api::student.student", ({ strapi }) => ({
   // Method 1: Creating an entirely custom action
   async index(ctx) {
     try {
+      //   console.log({ query: strapi.query('student').count({ name_contains: 'cem' }));
       let tmp = ctx.request.body;
 
       let resCode = validateLogin(tmp);
+      console.log({ a: JSON.parse(tmp) });
       if (resCode == 202) {
         //Accepted
         ctx.response.status = 202;
